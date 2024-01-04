0ba8503288e7762312c8af7b0a1ae59c18b84d4f
EduardoArle <eduardo.arle@idiv.de>
2024-01-04 03:30:10 +0200

    20240104

--- Combine_SHAP_range_info.R
+++ Combine_SHAP_range_info.R
@@ -29,6 +29,7 @@ sps_m_sel <- lapply(paste0(sps_list_sel,'_point_range_metrics.csv'), read.csv)
 names(sps_m_sel) <- sps_list_sel
 
 
+
 ##############################################
 ############## ALL SPECIES TEMP ##############
 ##############################################
@@ -39,15 +40,15 @@ setwd(wd_res_shap)
 
 #minT
 sps_minT <- lapply(sps_list_sel, function(x){read.csv(paste0(x,'_minT_.csv'))})
-names(sps_var) <- sps_list_sel
+names(sps_m_sel) <- sps_list_sel
 
 #meanT
 sps_meanT <- lapply(sps_list_sel, function(x){read.csv(paste0(x,'_meanT_.csv'))})
-names(sps_var) <- sps_list_sel
+names(sps_m_sel) <- sps_list_sel
 
 #maxT
 sps_maxT <- lapply(sps_list_sel, function(x){read.csv(paste0(x,'_maxT_.csv'))})
-names(sps_var) <- sps_list_sel
+names(sps_m_sel) <- sps_list_sel
 
 
 ## Select only rows representing presence
@@ -291,16 +292,16 @@ setwd(wd_res_shap)
 
 #minPPT
 sps_minPPT <- lapply(sps_list_sel, function(x){read.csv(paste0(x,'_minPPT_.csv'))})
-names(sps_var) <- sps_list_sel
+names(sps_m_sel) <- sps_list_sel
 
 #meanPPT
 sps_meanPPT <- lapply(sps_list_sel, 
                       function(x){read.csv(paste0(x,'_meanPPT_.csv'))})
-names(sps_var) <- sps_list_sel
+names(sps_m_sel) <- sps_list_sel
 
 #maxPPT
 sps_maxPPT <- lapply(sps_list_sel, function(x){read.csv(paste0(x,'_maxPPT_.csv'))})
-names(sps_var) <- sps_list_sel
+names(sps_m_sel) <- sps_list_sel
 
 
 ## Select only rows representing presence
@@ -429,61 +430,6 @@ text(0.10, 12.5, round(r2,3), col = '#1a9850', font = 2)
 dev.off()
 
 
-## Save plot image precipitation variables contribution per centralness
-setwd(wd_res)
-pdf(file = 'AllSpp_Precipitation_Centralness.pdf')
-
-#set parametres for plotting
-par(mar = c(5,5,5,5))
-
-#minPPT
-plot(sps_minPPT_SHAP_info_allSpp$centralness, cont_minPPT, 
-     pch = 19, cex = 0.4, col = '#fc8d5910',
-     ylab = 'Contribution minPPT',
-     xlab = 'Centralness')
-
-#fit linear model
-lin_mod_minPPT <- lm(cont_minPPT ~ sps_minPPT_SHAP_info_allSpp$centralness)
-abline(lin_mod_minPPT, col = '#fc8d59', lwd = 2)
-
-#get R^2
-r2 <- summary(lin_mod_minPPT)$r.squared
-text(0.05, 5, expression(bold(paste('R'^2*' = '), sep='')), col = '#fc8d59')
-text(0.15, 4.5, round(r2,3), col = '#ff8d59', font = 2)
-
-#meanPPT
-points(sps_meanPPT_SHAP_info_allSpp$centralness, cont_meanPPT, 
-       pch = 19, cex = 0.4, col = '#8c510a10')
-
-#fit linear model
-lin_mod_meanPPT <- lm(cont_meanPPT ~ sps_meanPPT_SHAP_info_allSpp$centralness)
-abline(lin_mod_meanPPT, col = '#8c510a', lwd = 2)
-
-#get R^2
-r2 <- summary(lin_mod_meanPPT)$r.squared
-text(0.05, 9, expression(bold(paste('R'^2*' = '), sep='')), col = '#8c510a')
-text(0.15, 8.5, round(r2,3), col = '#8c510a', font = 2)
-
-
-#maxPPT
-points(sps_maxPPT_SHAP_info_allSpp$centralness, cont_maxPPT, 
-       pch = 19, cex = 0.4, col = '#1a985010')
-
-#fit linear model
-lin_mod_maxPPT <- lm(cont_maxPPT ~ sps_maxPPT_SHAP_info_allSpp$centralness)
-abline(lin_mod_maxPPT, col = '#1a9850', lwd = 2)
-
-#get R^2
-r2 <- summary(lin_mod_maxPPT)$r.squared
-text(0.05, 13, expression(bold(paste('R'^2*' = '), sep='')), col = '#1a9850')
-text(0.15, 12.5, round(r2,3), col = '#1a9850', font = 2)
-
-dev.off()
-
-##############################################################################
-
-
-
 ## Save plot image precipitation variables contribution per relative polarwardness
 setwd(wd_res)
 pdf(file = 'AllSpp_Precipitation_RelPolar.pdf')
@@ -537,6 +483,61 @@ text(0.15, 12.5, round(r2,3), col = '#1a9850', font = 2)
 dev.off()
 
 
+## Save plot image precipitation variables contribution per centralness
+setwd(wd_res)
+pdf(file = 'AllSpp_Precipitation_Centralness.pdf')
+
+#set parametres for plotting
+par(mar = c(5,5,5,5))
+
+#minPPT
+plot(sps_minPPT_SHAP_info_allSpp$centralness, cont_minPPT, 
+     pch = 19, cex = 0.4, col = '#fc8d5910',
+     ylab = 'Contribution minPPT',
+     xlab = 'Centralness')
+
+#fit linear model
+lin_mod_minPPT <- lm(cont_minPPT ~ sps_minPPT_SHAP_info_allSpp$centralness)
+abline(lin_mod_minPPT, col = '#fc8d59', lwd = 2)
+
+#get R^2
+r2 <- summary(lin_mod_minPPT)$r.squared
+text(0.05, 5, expression(bold(paste('R'^2*' = '), sep='')), col = '#fc8d59')
+text(0.15, 4.5, round(r2,3), col = '#ff8d59', font = 2)
+
+#meanPPT
+points(sps_meanPPT_SHAP_info_allSpp$centralness, cont_meanPPT, 
+       pch = 19, cex = 0.4, col = '#8c510a10')
+
+#fit linear model
+lin_mod_meanPPT <- lm(cont_meanPPT ~ sps_meanPPT_SHAP_info_allSpp$centralness)
+abline(lin_mod_meanPPT, col = '#8c510a', lwd = 2)
+
+#get R^2
+r2 <- summary(lin_mod_meanPPT)$r.squared
+text(0.05, 9, expression(bold(paste('R'^2*' = '), sep='')), col = '#8c510a')
+text(0.15, 8.5, round(r2,3), col = '#8c510a', font = 2)
+
+
+#maxPPT
+points(sps_maxPPT_SHAP_info_allSpp$centralness, cont_maxPPT, 
+       pch = 19, cex = 0.4, col = '#1a985010')
+
+#fit linear model
+lin_mod_maxPPT <- lm(cont_maxPPT ~ sps_maxPPT_SHAP_info_allSpp$centralness)
+abline(lin_mod_maxPPT, col = '#1a9850', lwd = 2)
+
+#get R^2
+r2 <- summary(lin_mod_maxPPT)$r.squared
+text(0.05, 13, expression(bold(paste('R'^2*' = '), sep='')), col = '#1a9850')
+text(0.15, 12.5, round(r2,3), col = '#1a9850', font = 2)
+
+dev.off()
+
+
+
+
+
 ##############################################################################
 
 
@@ -551,39 +552,39 @@ for(i in 1:length(vars))
   
   #load results for this variable considering all monohemisferic species
   # setwd(wd_res_shap)
-  # sps_var <- lapply(sps_list_sel, function(x){
+  # sps_m_sel <- lapply(sps_list_sel, function(x){
   #                             read.csv(paste0(x,'_',var,'_.csv'))})
-  # names(sps_var) <- sps_list_sel
+  # names(sps_m_sel) <- sps_list_sel
   
   #select only rows representing presence
-  # sps_var_pr <- lapply(sps_var, function(x){x[x$Occurrence == 1,]})
+  # sps_m_sel_pr <- lapply(sps_m_sel, function(x){x[x$Occurrence == 1,]})
   
   #concatenate point and range info to SHAP results
-  # sps_var_SHAP_info <- list()
-  # for(j in 1:length(sps_var_pr))
+  # sps_m_sel_SHAP_info <- list()
+  # for(j in 1:length(sps_m_sel_pr))
   # {
-  #   sps_var_SHAP_info[[j]] <- cbind(sps_var_pr[[j]], 
+  #   sps_m_sel_SHAP_info[[j]] <- cbind(sps_m_sel_pr[[j]], 
   #                                   sps_m_sel[[j]][,c('key','datasetKey',
   #                                   'rangeSize','roundness','centralness',     
   #                                   'absPolarwardness','relPolarwardness')])
   # }
     
   #bind all rows in one table
-  # sps_var_SHAP_info2 <- rbindlist(sps_var_SHAP_info)
-  # sps_var_SHAP_info2 <- as.data.frame(sps_var_SHAP_info2)
+  # sps_m_sel_SHAP_info2 <- rbindlist(sps_m_sel_SHAP_info)
+  # sps_m_sel_SHAP_info2 <- as.data.frame(sps_m_sel_SHAP_info2)
 
   #calculate percentage contribution of the variable
-  # cont_var <- abs(sps_var_SHAP_info2[,9]) / 
-  #             (abs(sps_var_SHAP_info2[,9]) + abs(sps_var_SHAP_info2[,8])) * 100
+  # cont_var <- abs(sps_m_sel_SHAP_info2[,9]) / 
+  #             (abs(sps_m_sel_SHAP_info2[,9]) + abs(sps_m_sel_SHAP_info2[,8])) * 100
   
   ### Variable contribution per absolute polarwardness
-  # plot(sps_var_SHAP_info2$absPolarwardness, cont_var, 
+  # plot(sps_m_sel_SHAP_info2$absPolarwardness, cont_var, 
   #      pch = 19, cex = 0.4, col = 'gray80',
   #      ylab = paste0('Contribution ', var),
   #      xlab = 'Absolute Polarwardness')
   # 
   # #fit linear model
-  # lin_mod <- lm(cont_var ~ sps_var_SHAP_info2$absPolarwardness)
+  # lin_mod <- lm(cont_var ~ sps_m_sel_SHAP_info2$absPolarwardness)
   # abline(lin_mod, col = 'red', lwd = 2)
   # 
   # #get R^2
@@ -593,13 +594,13 @@ for(i in 1:length(vars))
   
   
   # ### Variable contribution per relative polarwardness
-  # plot(sps_var_SHAP_info2$relPolarwardness, cont_var, 
+  # plot(sps_m_sel_SHAP_info2$relPolarwardness, cont_var, 
   #      pch = 19, cex = 0.4, col = 'gray80',
   #      ylab = paste0('Contribution ', var),
   #      xlab = 'Relative Polarwardness')
   # 
   # #fit linear model
-  # lin_mod <- lm(cont_var ~ sps_var_SHAP_info2$relPolarwardness)
+  # lin_mod <- lm(cont_var ~ sps_m_sel_SHAP_info2$relPolarwardness)
   # abline(lin_mod, col = 'red', lwd = 2)
   # 
   # #get R^2
@@ -609,13 +610,13 @@ for(i in 1:length(vars))
   
   
   ### Variable contribution per centralness
-  # plot(sps_var_SHAP_info2$centralness, cont_var, 
+  # plot(sps_m_sel_SHAP_info2$centralness, cont_var, 
   #      pch = 19, cex = 0.4, col = 'gray80',
   #      ylab = paste0('Contribution ', var),
   #      xlab = 'Centralness')
   # 
   # #fit linear model
-  # lin_mod <- lm(cont_var ~ sps_var_SHAP_info2$centralness)
+  # lin_mod <- lm(cont_var ~ sps_m_sel_SHAP_info2$centralness)
   # abline(lin_mod, col = 'red', lwd = 2)
   # 
   # #get R^2
@@ -624,15 +625,15 @@ for(i in 1:length(vars))
   # text(0.16, 4, round(r2,3), col = 'red', font = 2)
   # 
 
-}
-
-?lm
-
-
-summary(sps_var_SHAP_info2$absPolarwardness)
-
-as.numeric((sps_var_SHAP_info2[,9]))
-
-a <- mapply(function(x, y) seq_len(x) + y,
-       c(a =  1, b = 2, c = 3),  # names from first
-       c(A = 10, B = 0, C = -10))
+# }
+# 
+# ?lm
+# 
+# 
+# summary(sps_m_sel_SHAP_info2$absPolarwardness)
+# 
+# as.numeric((sps_m_sel_SHAP_info2[,9]))
+# 
+# a <- mapply(function(x, y) seq_len(x) + y,
+#        c(a =  1, b = 2, c = 3),  # names from first
+#        c(A = 10, B = 0, C = -10))
