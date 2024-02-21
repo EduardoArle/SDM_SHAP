d37f67fa3a6a8aed97f5f60ad80864eab9d8ab13
EduardoArle <eduardo.arle@idiv.de>
2024-02-21 11:06:03 +0200

    20240221

--- Analise_results_mammals.R
+++ Analise_results_mammals.R
@@ -158,7 +158,8 @@ axis(side = 1,
 #add X axis
 axis(side = 2, 
      at = c(1:nrow(data_split[[i]])),
-     labels = data_split[[i]]$species, cex.axis = 0.8, padj = 0, las = 2, font = 3)
+     labels = data_split[[i]]$species,
+     cex.axis = 0.8, padj = 0, las = 2, font = 3)
 
 #add a dashed line to show 0
 abline(v = 0, lty = 'dotted', lwd = 2)
@@ -310,151 +311,163 @@ res_temp3$maxTp_value_C_colour[res_temp3$maxTp_value_C_bin == 4] <- '#de2d26'
 res_temp3$maxTp_value_C_colour[res_temp3$maxTp_value_C_bin == 5] <- '#a50f15'
 
 #classify species by range size (try also including roundness???)
-res_temp3$latAmplitude <- res_temp3$maxLat - res_temp3$minLat
-                          res_temp3$latAmplitude_bin <- NA
+res_temp3$rangeSize_bin <- NA
                         
-                        res_temp3$latAmplitude_bin[res_temp3$latAmplitude  < 1] <- 1
+res_temp3$rangeSize_bin[res_temp3$rangeSize < 100000] <- 1
                         
-                        res_temp3$latAmplitude_bin[res_temp3$latAmplitude >= 1 &
-                                                     res_temp3$latAmplitude < 5] <- 2
+res_temp3$rangeSize_bin[res_temp3$rangeSize >= 100000 &
+                        res_temp3$rangeSize < 500000] <- 2
                         
-                        res_temp3$latAmplitude_bin[res_temp3$latAmplitude >= 5 &
-                                                     res_temp3$latAmplitude < 10] <- 3
+res_temp3$rangeSize_bin[res_temp3$rangeSize >= 500000 &
+                        res_temp3$rangeSize < 1000000] <- 3
                         
-                        res_temp3$latAmplitude_bin[res_temp3$latAmplitude >=  10] <- 4
+res_temp3$rangeSize_bin[res_temp3$rangeSize >=  1000000] <- 4
                         
-                        #order the species from highest to lowest slope of min T
-                        res_temp4 <- res_temp3[order(res_temp3$minTslope_RP),] #order for the plot
+#order the species from highest to lowest slope of min T
+res_temp4 <- res_temp3[order(res_temp3$minTslope_C),] #order for the plot
                         
-                        #get the xlim for the plot
-                        res_temp4$minT_minVals <- res_temp4$minTslope_RP - res_temp4$minTSE_RP
-                        res_temp4$minT_maxVals <- res_temp4$minTslope_RP + res_temp4$minTSE_RP
+#get the xlim for the plot
+res_temp4$minT_minVals_C <- res_temp4$minTslope_C - res_temp4$minTSE_C
+res_temp4$minT_maxVals_C <- res_temp4$minTslope_C + res_temp4$minTSE_C
                         
-                        res_temp4$meanT_minVals <- res_temp4$meanTslope_RP - res_temp4$meanTSE_RP
-                        res_temp4$meanT_maxVals <- res_temp4$meanTslope_RP + res_temp4$meanTSE_RP
+res_temp4$meanT_minVals_C <- res_temp4$meanTslope_C - res_temp4$meanTSE_C
+res_temp4$meanT_maxVals_C <- res_temp4$meanTslope_C + res_temp4$meanTSE_C
                         
-                        res_temp4$maxT_minVals <- res_temp4$maxTslope_RP - res_temp4$maxTSE_RP
-                        res_temp4$maxT_maxVals <- res_temp4$maxTslope_RP + res_temp4$maxTSE_RP
+res_temp4$maxT_minVals_C <- res_temp4$maxTslope_C - res_temp4$maxTSE_C
+res_temp4$maxT_maxVals_C <- res_temp4$maxTslope_C + res_temp4$maxTSE_C
                         
-                        x_lim <- c(res_temp4$minT_minVals, res_temp4$minT_maxVals,
-                                   res_temp4$meanT_minVals, res_temp4$meanT_maxVals,
-                                   res_temp4$maxT_minVals, res_temp4$maxT_maxVals)
+x_lim <- c(res_temp4$maxT_minVals_C, res_temp4$maxT_minVals_C,
+           res_temp4$maxT_minVals_C, res_temp4$maxT_minVals_C,
+           res_temp4$maxT_minVals_C, res_temp4$maxT_minVals_C)
                         
+#### plot 20 species per plot ####
+data_split <- list()
                         
-                        #### plot 20 species per plot ####
-                        data_split <- list()
+data_split[[1]] <- res_temp4[c(1:20), ]
+data_split[[2]] <- res_temp4[c(21:40), ]
+data_split[[3]] <- res_temp4[c(41:60), ]
+data_split[[4]] <- res_temp4[c(61:80), ]
+data_split[[5]] <- res_temp4[c(81:100), ]
+data_split[[6]] <- res_temp4[c(101:120), ]
+data_split[[7]] <- res_temp4[c(121:140), ]
+data_split[[8]] <- res_temp4[c(141:160), ]
+data_split[[9]] <- res_temp4[c(161:180), ]
+data_split[[10]] <- res_temp4[c(181:200), ]
+data_split[[11]] <- res_temp4[c(201:220), ]
+data_split[[12]] <- res_temp4[c(221:240), ]
+data_split[[13]] <- res_temp4[c(241:260), ]
+data_split[[14]] <- res_temp4[c(261:280), ]
+data_split[[15]] <- res_temp4[c(281:300), ]
+data_split[[16]] <- res_temp4[c(301:320), ]
+data_split[[17]] <- res_temp4[c(321:340), ]
+data_split[[18]] <- res_temp4[c(341:360), ]
+data_split[[19]] <- res_temp4[c(361:374), ]
                         
-                        data_split[[1]] <- res_temp4[c(1:20), ]
-                        data_split[[2]] <- res_temp4[c(21:40), ]
-                        data_split[[3]] <- res_temp4[c(41:60), ]
-                        data_split[[4]] <- res_temp4[c(61:80), ]
-                        data_split[[5]] <- res_temp4[c(81:100), ]
-                        data_split[[6]] <- res_temp4[c(101:120), ]
-                        data_split[[7]] <- res_temp4[c(121:140), ]
-                        data_split[[8]] <- res_temp4[c(141:160), ]
-                        data_split[[9]] <- res_temp4[c(161:180), ]
-                        data_split[[10]] <- res_temp4[c(181:200), ]
-                        data_split[[11]] <- res_temp4[c(201:220), ]
-                        data_split[[12]] <- res_temp4[c(221:240), ]
-                        data_split[[13]] <- res_temp4[c(241:260), ]
-                        data_split[[14]] <- res_temp4[c(261:280), ]
-                        data_split[[15]] <- res_temp4[c(281:300), ]
-                        data_split[[16]] <- res_temp4[c(301:320), ]
-                        data_split[[17]] <- res_temp4[c(321:340), ]
-                        data_split[[18]] <- res_temp4[c(341:360), ]
-                        data_split[[19]] <- res_temp4[c(361:374), ]
+#### PLOT ####
+i=19
                         
-                        #### PLOT ####
-                        i=19
+#make an empty plot
+par(mar = c(5,10,1,1))
                         
-                        #make an empty plot
-                        par(mar = c(5,10,1,1))
+plot(1, type = "n", xlab = "", ylab = "", yaxt = 'n', xaxt = 'n', frame = F,
+     xlim = c(-1, 1),
+     ylim = c(0, nrow(data_split[[i]]))) 
                         
-                        plot(1, type = "n", xlab = "", ylab = "", yaxt = 'n', xaxt = 'n', frame = F,
-                             xlim = c(-1.5, 1.5),
-                             ylim = c(0, nrow(data_split[[i]]))) 
+#add X axis
+axis(side = 1, 
+     at = seq(-1, 1, 0.5),
+     cex.axis = 1, padj = 0, las =1)
                         
-                        #add X axis
-                        axis(side = 1, 
-                             at = seq(-1.5, 1.5, 0.5),
-                             cex.axis = 1, padj = 0, las =1)
+#add X axis
+axis(side = 2, 
+    at = c(1:nrow(data_split[[i]])),
+    labels = data_split[[i]]$species,
+    cex.axis = 0.8, padj = 0, las = 2, font = 3)
                         
-                        #add X axis
-                        axis(side = 2, 
-                             at = c(1:nrow(data_split[[i]])),
-                             labels = data_split[[i]]$species, cex.axis = 0.8, padj = 0, las = 2, font = 3)
+#add a dashed line to show 0
+abline(v = 0, lty = 'dotted', lwd = 2)
                         
-                        #add a dashed line to show 0
-                        abline(v = 0, lty = 'dotted', lwd = 2)
+#add points minT
+points(data_split[[i]]$minTslope_C, c(1:nrow(data_split[[i]])), pch = 19,
+       col = data_split[[i]]$minTp_value_C_colour,
+       cex = data_split[[i]]$rangeSize_bin)
                         
-                        #add points minT
-                        points(data_split[[i]]$minTslope_RP, c(1:nrow(data_split[[i]])), pch = 19,
-                               col = data_split[[i]]$minTp_value_RP_colour,
-                               cex = data_split[[i]]$latAmplitude_bin)
-                        
-                        #add lines minT
-                        for(j in 1:nrow(data_split[[i]]))
-                        {
-                          #make horizontal lines
-                          lines(x = c(data_split[[i]]$minT_minVals[j], data_split[[i]]$minT_maxVals[j]),
-                                y = c(j, j),
-                                lwd = 2, col = data_split[[i]]$minTp_value_RP_colour[j])
+
+#add lines minT
+for(j in 1:nrow(data_split[[i]]))
+{
+  #make horizontal lines
+  lines(x = c(data_split[[i]]$minT_minVals_C[j],
+              data_split[[i]]$minT_maxVals_C[j]),
+        y = c(j, j),
+        lwd = 2, col = data_split[[i]]$minTp_value_C_colour[j])
                           
-                          #make ticks in the ends of the lines
-                          lines(x = c(data_split[[i]]$minT_minVals[j], data_split[[i]]$minT_minVals[j]),
-                                y = c(j - 0.2, j + 0.2),
-                                lwd = 2, col = data_split[[i]]$minTp_value_RP_colour[j])
+  #make ticks in the ends of the lines
+  lines(x = c(data_split[[i]]$minT_minVals_C[j],
+              data_split[[i]]$minT_minVals_C[j]),
+        y = c(j - 0.2, j + 0.2),
+        lwd = 2, col = data_split[[i]]$minTp_value_C_colour[j])
                           
-                          lines(x = c(data_split[[i]]$minT_maxVals[j], data_split[[i]]$minT_maxVals[j]),
-                                y = c(j - 0.2, j + 0.2),
-                                lwd = 2, col = data_split[[i]]$minTp_value_RP_colour[j])
-                        }
+  lines(x = c(data_split[[i]]$minT_maxVals_C[j],
+              data_split[[i]]$minT_maxVals_C[j]),
+        y = c(j - 0.2, j + 0.2),
+        lwd = 2, col = data_split[[i]]$minTp_value_C_colour[j])
+}
+
+#add points meanT
+points(data_split[[i]]$meanTslope_C, c(1:nrow(data_split[[i]])), pch = 19,
+       col = data_split[[i]]$meanTp_value_C_colour,
+       cex = data_split[[i]]$rangeSize_bin)
                         
-                        #add points meanT
-                        points(data_split[[i]]$meanTslope_RP, c(1:nrow(data_split[[i]])), pch = 19,
-                               col = data_split[[i]]$meanTp_value_RP_colour,
-                               cex = data_split[[i]]$latAmplitude_bin)
                         
-                        
-                        #add lines meanT
-                        for(j in 1:nrow(data_split[[i]]))
-                        {
-                          #make horizontal lines
-                          lines(x = c(data_split[[i]]$meanT_minVals[j], data_split[[i]]$meanT_maxVals[j]),
-                                y = c(j, j),
-                                lwd = 2, col = data_split[[i]]$meanTp_value_RP_colour[j])
+#add lines meanT
+for(j in 1:nrow(data_split[[i]]))
+{
+  #make horizontal lines
+  lines(x = c(data_split[[i]]$meanT_minVals_C[j],
+              data_split[[i]]$meanT_maxVals_C[j]),
+        y = c(j, j),
+        lwd = 2, col = data_split[[i]]$meanTp_value_C_colour[j])
                           
-                          #make ticks in the ends of the lines
-                          lines(x = c(data_split[[i]]$meanT_minVals[j], data_split[[i]]$meanT_minVals[j]),
-                                y = c(j - 0.2, j + 0.2),
-                                lwd = 2, col = data_split[[i]]$meanTp_value_RP_colour[j])
+  #make ticks in the ends of the lines
+  lines(x = c(data_split[[i]]$meanT_minVals_C[j],
+              data_split[[i]]$meanT_minVals_C[j]),
+        y = c(j - 0.2, j + 0.2),
+        lwd = 2, col = data_split[[i]]$meanTp_value_C_colour[j])
                           
-                          lines(x = c(data_split[[i]]$meanT_maxVals[j], data_split[[i]]$meanT_maxVals[j]),
-                                y = c(j - 0.2, j + 0.2),
-                                lwd = 2, col = data_split[[i]]$meanTp_value_RP_colour[j])
-                        }
+   lines(x = c(data_split[[i]]$meanT_maxVals_C[j],
+               data_split[[i]]$meanT_maxVals_C[j]),
+        y = c(j - 0.2, j + 0.2),
+        lwd = 2, col = data_split[[i]]$meanTp_value_C_colour[j])
+}
                         
                         
-                        #add points maxT
-                        points(data_split[[i]]$maxTslope_RP, c(1:nrow(data_split[[i]])), pch = 19,
-                               col = data_split[[i]]$maxTp_value_RP_colour,
-                               cex = data_split[[i]]$latAmplitude_bin)
+#add points maxT
+points(data_split[[i]]$maxTslope_C, c(1:nrow(data_split[[i]])), pch = 19,
+       col = data_split[[i]]$maxTp_value_C_colour,
+       cex = data_split[[i]]$rangeSize_bin)
                         
                         
-                        #add lines maxT
-                        for(j in 1:nrow(data_split[[i]]))
-                        {
-                          #make horizontal lines
-                          lines(x = c(data_split[[i]]$maxT_minVals[j], data_split[[i]]$maxT_maxVals[j]),
-                                y = c(j, j),
-                                lwd = 2, col = data_split[[i]]$maxTp_value_RP_colour[j])
+#add lines maxT
+for(j in 1:nrow(data_split[[i]]))
+{
+  #make horizontal lines
+  lines(x = c(data_split[[i]]$maxT_minVals_C[j],
+              data_split[[i]]$maxT_maxVals_C[j]),
+        y = c(j, j),
+        lwd = 2, col = data_split[[i]]$maxTp_value_C_colour[j])
                           
-                          #make ticks in the ends of the lines
-                          lines(x = c(data_split[[i]]$maxT_minVals[j], data_split[[i]]$maxT_minVals[j]),
-                                y = c(j - 0.2, j + 0.2),
-                                lwd = 2, col = data_split[[i]]$maxTp_value_RP_colour[j])
+  #make ticks in the ends of the lines
+  lines(x = c(data_split[[i]]$maxT_minVals_C[j],
+              data_split[[i]]$maxT_minVals_C[j]),
+        y = c(j - 0.2, j + 0.2),
+        lwd = 2, col = data_split[[i]]$maxTp_value_C_colour[j])
                           
-                          lines(x = c(data_split[[i]]$maxT_maxVals[j], data_split[[i]]$maxT_maxVals[j]),
-                                y = c(j - 0.2, j + 0.2),
-                                lwd = 2, col = data_split[[i]]$maxTp_value_RP_colour[j])
-                        }
+  lines(x = c(data_split[[i]]$maxT_maxVals_C[j],
+              data_split[[i]]$maxT_maxVals_C[j]),
+        y = c(j - 0.2, j + 0.2),
+        lwd = 2, col = data_split[[i]]$maxTp_value_C_colour[j])
+}
+
+10h58
+
