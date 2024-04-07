8b03b4bb538c13debc30ebe14988aa969fde133e
EduardoArle <eduardo.arle@idiv.de>
2024-04-07 13:38:54 +0300

    20240407

--- Fig_1.R
+++ Fig_1.R
@@ -2,12 +2,12 @@
 library(rnaturalearth)
 
 #list wds
-wd_tables <- '/Users/carloseduardoaribeiro/Documents/Post-doc/SHAP/Manuscript/Methods/Things for figure'
+wd_tables <- '/Users/carloseduardoaribeiro/Documents/Post-doc/SHAP/Mammals/Manuscript/Figures/Fig 1/Things for the figure'
 
 #set seed
 set.seed(69)
 
-##### PLOT GRAPH ######
+##### GRAPH ######
 
 #create data for plot
 varContr <- c(rnorm(10, mean = 0, sd = 0.15),
@@ -22,22 +22,8 @@ relPolewardness <- sort(runif(n = 50, min = 0, max = 1))
 ylim <- c(-0.2, 0.7)
 xlim <- c(0, 1)
 
-#set parametres for plotting
-par(mar = c(5,5,5,5), pty="s")
 
-#minT
-plot(relPolewardness, varContr, 
-     pch = 19, cex = 0.8, col = '#0000FF25',
-     ylab = 'Temperature contribution',
-     xlab = 'Relative polarwardness',
-     ylim = c(ylim[1], ylim[2]))
-
-#fit linear model
-lin_mod_minT <- lm(varContr ~ relPolewardness)
-abline(lin_mod_minT, col = '#0000FF', lwd = 3)
-
-
-##### PLOT MAP #####
+##### MAP #####
 
 #load world map
 world <- ne_countries(returnclass = "sf")
@@ -59,8 +45,16 @@ lon <- runif(n = 50,
              min = st_bbox(poly_sf)$xmin,
              max = st_bbox(poly_sf)$xmax)
 
+coords <- as.data.frame(cbind(lon, lat))
+
+points_sf <- st_as_sf(coords, coords = c('lon', 'lat'), crs = crs(world))
+
 #check one by one and put the ones out inside of the range
 #I know, not a smart not efficient solution. but it gives me pleasure.
+
+
+##############################
+
 lon[1] <- lon[1] + 2
 lon[2] <- lon[2] - 3
 lon[3] <- lon[3] + 1
@@ -84,9 +78,6 @@ coords <- as.data.frame(cbind(lon, lat))
 
 points_sf <- st_as_sf(coords, coords = c('lon', 'lat'), crs = crs(world))
 
-#plot map
-plot(poly_sf, lwd = 3, border = '#707070', col = '#F0F0F0')
-plot(points_sf, add = T, pch = 19, cex = 0.8, col = '#0000FF25')
 
 plot(points_sf[1,], add = T, pch = 19, cex = 0.8, col = '#FF0000')
 plot(points_sf[2,], add = T, pch = 19, cex = 0.8, col = '#FF0000')
@@ -140,6 +131,76 @@ plot(points_sf[49,], add = T, pch = 19, cex = 0.8, col = '#FF0000')
 plot(points_sf[50,], add = T, pch = 19, cex = 0.8, col = '#FF0000')
 
 
+########################
+
 #save new coordinates
 setwd(wd_tables)
 write.csv(coords, 'Coordinates_points_large_map.csv', row.names = F)
+
+#make boxes for the map
+
+#make a box around the range
+ext <- st_bbox(poly_sf)  #get min and max coord values of the range
+
+x_mar <- abs(ext[3] - ext[1]) / 3 #get the values for the margin around the box
+y_mar <- abs(ext[4] - ext[2]) / 3
+
+box_df <- data.frame(x = ext[c(1,3,3,1,1)] + x_mar * c(-1,1,1,-1,-1), 
+                     y = ext[c(4,4,2,2,4)] + y_mar * c(1,1,-1,-1,1))
+
+box <- st_as_sfc(          #make the box    
+  st_bbox(st_as_sf(box_df, coords = c('x', 'y'), crs = crs(poly_sf))))
+
+#make a larger box around the box
+ext <- st_bbox(poly_sf)  #get min and max coord values of the range
+
+x_mar <- abs(ext[3] - ext[1]) / 1.4 
+y_mar <- abs(ext[4] - ext[2]) / 1.4
+
+big_box_df <- data.frame(x = ext[c(1,3,3,1,1)] + x_mar * c(-1,1,1,-1,-1), 
+                     y = ext[c(4,4,2,2,4)] + y_mar * c(1,1,-1,-1,1))
+
+big_box <- st_as_sfc(          #make the box    
+  st_bbox(st_as_sf(big_box_df, coords = c('x', 'y'), crs = crs(poly_sf))))
+
+
+###### PLOT ######
+
+
+#include variable contribution info in the attribute table
+points_sf$varContr <- varContr
+
+#normalize variable contribution from 0 to 5 (for size)
+points_sf$varContrNormal <- 
+  round(((varContr + abs(min(varContr))) * 2) + 0.7, 2)
+
+#set parametres for plotting
+par(mar = c(4,4,4,4), pty="s", mfrow = c(1,1))
+
+#minT
+plot(relPolewardness, varContr, 
+     pch = 19, cex = 0.8, col = '#0000FF25',
+     ylab = 'Temperature contribution',
+     xlab = 'Relative polarwardness',
+     ylim = c(ylim[1], ylim[2]))
+
+#fit linear model
+lin_mod_minT <- lm(varContr ~ relPolewardness)
+abline(lin_mod_minT, col = '#0000FF', lwd = 3)
+
+
+#set parametres for plotting
+par(mar = c(0,0,0,0), pty="s", mfrow = c(1,1))
+
+#plot map
+plot(big_box, border = NA)
+plot(box, add = T)
+plot(poly_sf, lwd = 3, border = '#707070', col = '#F0F0F0', add = T)
+plot(st_geometry(points_sf),
+     add = T, pch = 19, cex = points_sf$varContrNormal,
+     col = '#0000FF80')
+text(-9, 18, 'Longitude', srt = 90)
+points((st_bbox(box)[1] + st_bbox(box)[3])/2, st_bbox(box)[2] - 0.5,
+       pch = '|',
+       cex = 0.8)
+
